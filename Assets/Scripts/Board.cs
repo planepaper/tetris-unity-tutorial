b1abb50d8baff40d2ab537be85a634488ce9dbe7
planepaper <eddysonkr@gmail.com>
2022-02-11 23:30:08 +0900

    FEAT: Make Pieces Move with WASD
    - Make Move Method on 'TetrisPiece' class.
    - Append Update code to color tile.
    - Modify variable name in right convention.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,7 +7,17 @@ public class Board : MonoBehaviour
     [SerializeField]
     private Tetromino[] tetrominoes;
     private TetrisPiece activePiece;
-    private Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+    private readonly Vector3Int _SPAWNPOSITION = new Vector3Int(-1, 8, 0);
+    private readonly Vector2Int _BOARDSIZE = new Vector2Int(10, 20);
+
+    private RectInt OffSet
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-_BOARDSIZE.x / 2, -_BOARDSIZE.y / 2);
+            return new RectInt(position, _BOARDSIZE);
+        }
+    }
 
     private void Awake()
     {
@@ -16,7 +26,6 @@ public class Board : MonoBehaviour
         {
             tetromino.SetTakingCoordinate();
         }
-
     }
 
     private void Start()
@@ -24,21 +33,98 @@ public class Board : MonoBehaviour
         SpawnActivePiece();
     }
 
+    private void Update()
+    {
+        ClearPieceTile(activePiece);
+
+        Vector2Int direction = Vector2Int.zero;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector2Int.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2Int.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector2Int.down;
+        }
+        else
+        {
+        }
+
+        Vector3Int newPosition = activePiece.position;
+        newPosition.x += direction.x;
+        newPosition.y += direction.y;
+
+        if (IsValidPosition((Vector3Int)newPosition))
+        {
+            activePiece.Move(newPosition);
+        }
+
+        SetPieceTile();
+    }
+
     public void SpawnActivePiece()
     {
         int randomIndex = Random.Range(0, tetrominoes.Length);
         activePiece = new TetrisPiece(tetrominoes[randomIndex]);
 
-        SetPieceTile(activePiece);
+        SetPieceTile();
     }
 
-    public void SetPieceTile(TetrisPiece activePiece)
+    public void SetPieceTile()
     {
-        Vector2Int[] takingCoordinates = activePiece.tetromino.takingCoordinate;
-        for (int i = 0; i < takingCoordinates.Length; i++)
+        foreach (var takingCoordinate in activePiece.tetromino.takingCoordinates)
         {
-            Vector3Int tilePosition = (Vector3Int)takingCoordinates[i] + activePiece.position;
+            Vector3Int tilePosition = (Vector3Int)takingCoordinate + activePiece.position;
             tilemap.SetTile(tilePosition, activePiece.tetromino.tile);
         }
+    }
+
+    public void ClearPieceTile(TetrisPiece piece)
+    {
+        foreach (var takingCoordinate in activePiece.tetromino.takingCoordinates)
+        {
+            Vector3Int tilePosition = (Vector3Int)takingCoordinate + activePiece.position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    //TODO: HARDDROP
+    // private Vector3Int HardDrop()
+    // {
+    //     Vector3Int newPosition = activePiece.position;
+    //     while (IsValidPosition(newPosition))
+    //     {
+    //         newPosition.x += Vector2Int.down.x;
+    //         newPosition.y += Vector2Int.down.y;
+    //     }
+
+
+    //     activePiece.Move(newPosition);
+    // }
+
+    public bool IsValidPosition(Vector3Int position)
+    {
+        RectInt offset = OffSet;
+
+        foreach (var takingCoordinate in activePiece.tetromino.takingCoordinates)
+        {
+            Vector3Int tilePosition = (Vector3Int)takingCoordinate + position;
+
+            if (!offset.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            if (tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Assets/Scripts/TetrisPiece.cs
+++ Assets/Scripts/TetrisPiece.cs
@@ -19,11 +19,11 @@ public class Tetromino
     [SerializeField]
     private PieceAlphabet pieceAlphabet;
     public Tile tile;
-    public Vector2Int[] takingCoordinate { get; private set; }
+    public Vector2Int[] takingCoordinates { get; private set; }
 
     public void SetTakingCoordinate()
     {
-        takingCoordinate = Data.Cells[pieceAlphabet];
+        takingCoordinates = Data.Cells[pieceAlphabet];
     }
 }
 
@@ -31,10 +31,16 @@ public class TetrisPiece : MonoBehaviour
 {
     [SerializeField]
     public Tetromino tetromino { get; private set; }
-    public Vector3Int position { get; private set; } = new Vector3Int(-1, 8, 0);
+    public Vector3Int position = new Vector3Int(-1, 8, 0);
 
     public TetrisPiece(Tetromino tetromino)
     {
         this.tetromino = tetromino;
+    }
+
+    public void Move(Vector3Int newPosition)
+    {
+        position.x = newPosition.x;
+        position.y = newPosition.y;
     }
 }
